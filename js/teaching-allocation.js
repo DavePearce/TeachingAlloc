a6c1b48710144c82f8f403aa9703efcf5496249d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-08-27 11:58:26 +1000

    Well, making some progress ... but need to figure out exactly what I want to show.

--- js/teaching-allocation.js
+++ js/teaching-allocation.js
@@ -43,7 +43,7 @@ function calculate_course_load(course_record) {
 /**
  * Calculate the teaching load for a given course allocation.
  */
-function calculate_lecturing_load(allocation) {
+function calculate_teaching_load(allocation) {
   var load = 0;
   for(var i=0;i!=allocation.length;++i) {
       var allocation_record = allocation[i];
@@ -65,7 +65,16 @@ function calculate_lecturing_load(allocation) {
 }
 
 function calculate_supervision_load(allocation) {
-  return 0.5;
+    var load = 0;
+
+    for(var i=0;i!=allocation.length;++i) {
+	var allocation_record = allocation[i];
+	var supervision_load = allocation_record.load;          
+	load = load + supervision_load;
+    }
+    
+    // NB: max supervision is 2.5PG
+    return Math.min(load,2.5) / 2.5;
 }
 
 /**
@@ -95,18 +104,36 @@ function calculate_staff_allocation(allocation_records,staff) {
 	    staff_record.allocation.push({name: course_name, load: course_load, coordinator: course_coordinator});
 	}
     }
-    
-    // Third, flatten into an array and sort
-    var sorted_records = [];
-    var count = 0;
-    for(var staff_name in staff_records) {
-	var staff_record = staff_records[staff_name];
-	staff_record.teaching_load = calculate_lecturing_load(staff_record.allocation);
-	staff_record.supervision_load = calculate_supervision_load();
-	sorted_records[count++] = staff_record;
+    return staff_records;
+}
+
+/**
+ * Calculate the allocation of courses to individual staff members,
+ * including their current workload allocation.
+ */
+function calculate_staff_supervision(supervision_records,staff) {
+    var staff_records = {};
+
+    // First, initialise staff records
+    for(var i=0;i!=staff.length;++i) {
+    	var staff_name = staff[i].name;
+    	staff_records[staff_name] = { name: staff_name, allocation: [] };
     }
-    // done
-    return sorted_records;
+
+    // Second, merge records together for each staff member.
+    for(var i=0;i!=supervision_records.length;i=i+1) {
+    	var supervision_record = supervision_records[i];
+    	var student_name = supervision_record.student;
+    	var staff_name = supervision_record.supervisor;
+    	var supervision_load = supervision_record.load;
+
+    	if(staff_name in staff_records) {
+    	    // Sanity check staff member actually registered
+    	    var staff_record = staff_records[staff_name];
+    	    staff_record.allocation.push({name: student_name, load: supervision_load});
+    	}
+    }
+    return staff_records;
 }
 
 /**
@@ -137,15 +164,7 @@ function calculate_course_allocation(allocation_records,courses) {
 	    course_record.load += course_load;
 	} 
     }
-    
-    // Third, flatten into an array and sort
-    var sorted_records = [];
-    var count = 0;
-    for(var course_name in course_records) {
-	sorted_records[count++] = course_records[course_name];
-    }
-    // done
-    return sorted_records;
+    return course_records;
 }
 
 // ===============================================================
@@ -161,7 +180,7 @@ function to_allocation_string(records) {
     if(i != 0) { result = result + ", "; }
     var record = records[i];
     result = result + record.name;
-    if(record.coordinator) {
+    if("coordinator" in record && record.coordinator) {
        result = result + "*";
     }
     result = result + " (" + (record.load * 100) + "%)";   
@@ -200,45 +219,91 @@ function addRow(table,value) {
  * 4) List of allocations where reach record allocates a given staff
  * member to a given course.
  */
-function populateTables(staff,courses,supervision,allocation) {
-
+function populateTables(staff,courses,students,supervision,allocation) {
+    
     // First, populate the staff table
     var staffTable = document.getElementById("staff");
     $.each(staff,function(key,value){
-      fte = (calculate_effective_fte(value.fte,value.buyout,value.leave) * 100) + "%";
-      research = (value.research*100)+"%";
-      teaching = (value.teaching*100)+"%";
-      admin = (value.admin*100)+"%";
-      addRow(staffTable,value.name,fte,research,teaching,admin,value.notes);
+      var fte = (calculate_effective_fte(value.fte,value.buyout,value.leave) * 100) + "%";
+      var research = (value.research*100)+"%";
+      var teaching = (value.teaching*100)+"%";
+      var admin = (value.admin*100)+"%";
+      addRow(staffTable,
+	     value.name,
+	     fte,
+	     research,
+	     teaching,
+	     admin,
+	     value.notes);
     });
 
     // Second, populate the course table
     var courseTable = document.getElementById("courses");
     $.each(courses,function(key,value){
-       addRow(courseTable,value.code,value.number,value.title,value.trimester,value.expected,value.offered);
+       addRow(courseTable,
+	      value.id,
+	      value.title,
+	      value.trimester,
+	      value.expected,
+	      value.offered);
     });
 
-    // Third, populate the supervision table
+    // Third, populate the students table
+    var studentsTable = document.getElementById("students");
+    $.each(students,function(key,value){
+       addRow(studentsTable,
+	      value.name,
+	      value.degree);
+    });
+
+    // Fourth, populate the supervision table
     var supervisionTable = document.getElementById("supervision");
-    $.each(supervision,function(key,value){
-       addRow(supervisionTable,value.name,value.degree,value.supervisors);
+    var supervision_allocation = calculate_staff_supervision(supervision,staff);
+    $.each(supervision_allocation,function(key,value){
+	var load = calculate_supervision_load(value.allocation);
+    	addRow(supervisionTable,
+	       value.name,
+	       Math.round(load*100)+"%",
+	       to_allocation_string(value.allocation));
     });
 
-    // Fourth, populate the allocation table
+    // Fifth, populate the allocation table
     var staff_allocation = calculate_staff_allocation(allocation,staff);
     var course_allocation = calculate_course_allocation(allocation,courses);
         
     var allocationTable = document.getElementById("staff-allocation");
     $.each(staff_allocation,function(key,value){       
-       var load = Math.round((value.teaching_load + value.supervision_load) * 100);
-       var supervision = Math.round(value.supervision_load * 100);
-       var teaching = Math.round(value.teaching_load * 100);
-       addRow(allocationTable,value.name,load + "% (" + teaching + "+" + supervision + ")",to_allocation_string(value.allocation));
+	var load = calculate_teaching_load(value.allocation);
+	addRow(allocationTable,
+	       value.name,
+	       Math.round(load*100) + "%",
+	       to_allocation_string(value.allocation));
     });
-
+    
     allocationTable = document.getElementById("course-allocation");
     $.each(course_allocation,function(key,value){       
-       addRow(allocationTable,value.name,value.load,to_allocation_string(value.allocation));
+       addRow(allocationTable,
+	      value.name,
+	      value.load,
+	      to_allocation_string(value.allocation));
+    });
+
+    // Finally, populate the overall summary table
+    var summaryTable = document.getElementById("summary");
+    $.each(staff,function(key,value){
+	var staff_name = value.name;
+        var fte = calculate_effective_fte(value.fte,value.buyout,value.leave);
+        var admin_load = value.admin;
+	var teaching_load = calculate_teaching_load(staff_allocation[staff_name].allocation);
+	var supervision_load = calculate_supervision_load(supervision_allocation[staff_name].allocation);
+	var overall_load = (admin_load + teaching_load + supervision_load) / fte;
+	addRow(summaryTable,
+	       staff_name,
+	       (Math.round(fte*100)/100),
+	       Math.round(100*admin_load)+"%",
+	       Math.round(100*teaching_load)+"%",
+	       Math.round(100*supervision_load)+"%",
+	       Math.round(100*overall_load)+"%");	
     });
 }
 
